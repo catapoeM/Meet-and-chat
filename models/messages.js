609cdd81df368e9f0e8ae234cac7beb10f1b2f62
catapoeM <catalin_poenaru95@yahoo.com>
2022-07-31 18:55:55 +0200

    DB error done!

--- models/messages.js
+++ models/messages.js
@@ -4,7 +4,8 @@ const messagesSchema = new mongoose.Schema({
   message: String,
   userName: String,
   date: String,
-  likes: Number
+  likes: Number,
+  whoLiked: [String]
 })
 
 module.exports = mongoose.model('Messages', messagesSchema)
